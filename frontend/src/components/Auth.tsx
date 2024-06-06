deea457a8b52757cbfa02b843e22506f493fdc10
Anupoju <bhavya.anupoju@gmail.com>
2024-06-06 11:32:08 +0530

    Added New pages and components

--- frontend/src/components/Auth.tsx
+++ frontend/src/components/Auth.tsx
@@ -4,7 +4,6 @@ import { Link, useNavigate } from "react-router-dom"
 import axios from "axios";
 import { BACKEND_URL } from "../config";
 
-
 export const Auth = ({type}: {type: "signup" | "signin"}) => {
 
     const navigate = useNavigate()
@@ -13,7 +12,6 @@ export const Auth = ({type}: {type: "signup" | "signin"}) => {
         username: "",
         password:""
     }) 
-
 
     async function sendRequest(){
         try {
@@ -60,10 +58,8 @@ export const Auth = ({type}: {type: "signup" | "signin"}) => {
                     })
                 }} />
                 <button onClick={sendRequest} type="button" className="mt-8 text-white w-full bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700">{type === "signup" ? "Sign up" : "Sign in"}</button>
-
                 </div>
-                </div>
-                
+                </div>   
             </div>
         </div>
     )
@@ -76,11 +72,10 @@ interface LabelledInputType{
     type?: string;
 }
 
-
 function LabelledInput({ label, placeholder, onChange, type}: LabelledInputType){
     return(
     <div>
-        <label className="block mb-2 text-sm font-semibold text-gray-900 dark:text-white pt-4">{label}</label>
+        <label className="block mb-2 text-sm text-black font-semibold pt-4">{label}</label>
         <input onChange={onChange} type={type || "text"} id="first_name" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder={placeholder} required />
     </div>
 
