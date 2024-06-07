c16267ea45873b6284a238a98e4dae19b0bc0709
Anupoju <anupoju.bhavya.priya@accenture.com>
2024-06-07 15:13:50 +0530

    modified useBlog hook

--- frontend/src/hooks/index.ts
+++ frontend/src/hooks/index.ts
@@ -16,6 +16,9 @@ export const useBlog = ({ id }: { id: string }) => {
     const [loading, setLoading] = useState(true);
     const [blog, setBlog] = useState<Blog>();
 
+// check
+
+
     useEffect(() => {
         axios.get(`${BACKEND_URL}/api/v1/blog/${id}`, {
             headers: {
