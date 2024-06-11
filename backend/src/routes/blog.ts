95ca99b942f4054a2b651bb6e8df6166c24f49f4
Anupoju <anupoju.bhavya.priya@accenture.com>
2024-06-11 15:32:06 +0530

    Latest Changes

--- backend/src/routes/blog.ts
+++ backend/src/routes/blog.ts
@@ -1,49 +1,54 @@
-import {Context, Hono} from "hono";
-import { PrismaClient } from '@prisma/client/edge'
+import { createBlogInput, updateBlogInput } from "@bhavya_anupoju/brew-common";
+import { PrismaClient } from "@prisma/client/edge";
 import { withAccelerate } from "@prisma/extension-accelerate";
+import { Hono } from "hono";
 import { verify } from "hono/jwt";
 
-
 export const blogRouter = new Hono<{
-    Bindings:{
-      DATABASE_URL: string;
-      JWT_SECRET: string;
+    Bindings: {
+        DATABASE_URL: string;
+        JWT_SECRET: string;
+    }, 
+    Variables: {
+        userId: string;
     }
-    Variables:{
-        userId:string
-    }
-  }>();
+}>();
 
-//the extraction of the jwt takes place here and then the passed to the routes below
-blogRouter.use("/*", async (c,next) => {
-    // making the authHeader as string even if its passed or not to avoid type error
+blogRouter.use("/*", async (c, next) => {
     const authHeader = c.req.header("authorization") || "";
     try {
         const user = await verify(authHeader, c.env.JWT_SECRET);
-        if(user) {
+        if (user) {
             c.set("userId", user.id);
             await next();
-        }else {
+        } else {
             c.status(403);
             return c.json({
                 message: "You are not logged in"
             })
         }
-    } catch (e) {
+    } catch(e) {
         c.status(403);
         return c.json({
             message: "You are not logged in"
         })
     }
+});
 
-})
-
-//Creation of the blog post
 blogRouter.post('/', async (c) => {
     const body = await c.req.json();
-    const authorId = c.get("userId")
+    const { success } = createBlogInput.safeParse(body);
+    if (!success) {
+        c.status(411);
+        return c.json({
+            message: "Inputs not correct"
+        })
+    }
+
+    const authorId = c.get("userId");
     const prisma = new PrismaClient({
-        datasourceUrl: c.env.DATABASE_URL,}).$extends(withAccelerate())
+      datasourceUrl: c.env.DATABASE_URL,
+    }).$extends(withAccelerate())
 
     const blog = await prisma.blog.create({
         data: {
@@ -52,72 +57,94 @@ blogRouter.post('/', async (c) => {
             authorId: Number(authorId)
         }
     })
+
     return c.json({
         id: blog.id
     })
-  })
-  
+})
 
-//Updation of the blog post
-blogRouter.put('/', async(c) => {
+blogRouter.put('/', async (c) => {
     const body = await c.req.json();
+    const { success } = updateBlogInput.safeParse(body);
+    if (!success) {
+        c.status(411);
+        return c.json({
+            message: "Inputs not correct"
+        })
+    }
+
     const prisma = new PrismaClient({
-        datasourceUrl: c.env.DATABASE_URL,}).$extends(withAccelerate())
+      datasourceUrl: c.env.DATABASE_URL,
+    }).$extends(withAccelerate())
 
     const blog = await prisma.blog.update({
         where: {
             id: body.id
-        },
+        }, 
         data: {
             title: body.title,
-            content: body.content,
+            content: body.content
         }
     })
+
     return c.json({
         id: blog.id
     })
-  })
-  
-//Todo: addPagination
-  blogRouter.get('/bulk', async (c) => {
-    const body = await c.req.json();
-    const prisma = new PrismaClient({
-        datasourceUrl: c.env.DATABASE_URL,}).$extends(withAccelerate())
+})
 
-    const blogs = await prisma.blog.findMany();
+// Todo: add pagination
+blogRouter.get('/bulk', async (c) => {
+    const prisma = new PrismaClient({
+        datasourceUrl: c.env.DATABASE_URL,
+    }).$extends(withAccelerate())
+    const blogs = await prisma.blog.findMany({
+        select: {
+            content: true,
+            title: true,
+            id: true,
+            author: {
+                select: {
+                    name: true
+                }
+            }
+        }
+    });
 
     return c.json({
         blogs
     })
-  })
+})
 
-//Viewing the blog post
-blogRouter.get('/:id', async(c) => {
-    const id = await c.req.param("id");
+blogRouter.get('/:id', async (c) => {
+    const id = c.req.param("id");
     const prisma = new PrismaClient({
-        datasourceUrl: c.env.DATABASE_URL,}).$extends(withAccelerate())
+      datasourceUrl: c.env.DATABASE_URL,
+    }).$extends(withAccelerate())
 
     try {
         const blog = await prisma.blog.findFirst({
             where: {
                 id: Number(id)
+            },
+            select: {
+                id: true,
+                title: true,
+                content: true,
+                author: {
+                    select: {
+                        name: true
+                    }
+                }
             }
         })
+    
         return c.json({
             blog
-        })
-        
-    } catch (e) {
-        c.status(411);
+        });
+    } catch(e) {
+        c.status(411); // 4
         return c.json({
-            message: "Error while fetching the blog post"
-        })        
+            message: "Error while fetching blog post"
+        });
     }
-
-    
-  })
-  
-
-
-//If the context(c) does not have the variables that we need, we need to explicitly add then 
-  
+})
--- backend/src/routes/user.ts
+++ backend/src/routes/user.ts
@@ -1,20 +1,20 @@
-import {Hono} from "hono";
+import { Hono } from "hono";
 import { PrismaClient } from '@prisma/client/edge'
 import { withAccelerate } from '@prisma/extension-accelerate'
-import {decode, sign, verify } from 'hono/jwt'
-import { signupInput } from "@bhavya_anupoju/brew-common";
+import { sign } from 'hono/jwt'
+import { signupInput, signinInput } from "@bhavya_anupoju/brew-common";
 
 export const userRouter = new Hono<{
-    Bindings:{
-      DATABASE_URL: string;
-      JWT_SECRET: string;
+    Bindings: {
+        DATABASE_URL: string;
+        JWT_SECRET: string;
     }
-  }>()
+}>();
 
 userRouter.post('/signup', async (c) => {
     const body = await c.req.json();
-    const {success} = signupInput.safeParse(body)
-    if(!success){
+    const { success } = signupInput.safeParse(body);
+    if (!success) {
         c.status(411);
         return c.json({
             message: "Inputs not correct"
@@ -22,9 +22,9 @@ userRouter.post('/signup', async (c) => {
     }
     const prisma = new PrismaClient({
       datasourceUrl: c.env.DATABASE_URL,
-  }).$extends(withAccelerate())
+    }).$extends(withAccelerate())
   
-    try{
+    try {
       const user = await prisma.user.create({
         data: {
           username: body.username,
@@ -32,51 +32,54 @@ userRouter.post('/signup', async (c) => {
           name: body.name
         }
       })
-  
       const jwt = await sign({
         id: user.id
-      }, c.env.JWT_SECRET)
-      
+      }, c.env.JWT_SECRET);
+  
       return c.text(jwt)
-    }catch(e){
+    } catch(e) {
+      console.log(e);
       c.status(411);
       return c.text('Invalid')
     }
-  
   })
   
-  //signIn user
   
   userRouter.post('/signin', async (c) => {
     const body = await c.req.json();
+    const { success } = signinInput.safeParse(body);
+    if (!success) {
+        c.status(411);
+        return c.json({
+            message: "Inputs not correct"
+        })
+    }
+
     const prisma = new PrismaClient({
       datasourceUrl: c.env.DATABASE_URL,
-  }).$extends(withAccelerate())
+    }).$extends(withAccelerate())
   
-    try{
+    try {
       const user = await prisma.user.findFirst({
         where: {
           username: body.username,
           password: body.password,
-          name: body.name
         }
       })
-  
-      if(!user){
-        c.status(403);//unauthorized
+      if (!user) {
+        c.status(403);
         return c.json({
           message: "Incorrect creds"
         })
       }
-        const jwt = await sign({
+      const jwt = await sign({
         id: user.id
-      }, c.env.JWT_SECRET)
-      
+      }, c.env.JWT_SECRET);
+  
       return c.text(jwt)
-    }catch(e){
+    } catch(e) {
+      console.log(e);
       c.status(411);
       return c.text('Invalid')
     }
-  
   })
-  
